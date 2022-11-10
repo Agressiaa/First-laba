cb66cfc306bdf29fce955460b44c6107ee49e800
Pavel <117574203+Agressiaa@users.noreply.github.com>
2022-11-10 19:55:56 +0300

    Update Dot.java

--- firsstProject/src/Sobolevskiy6group/Dot.java
+++ firsstProject/src/Sobolevskiy6group/Dot.java
@@ -31,4 +31,17 @@ public class Dot {
                 ", y=" + y +
                 '}';
     }
+    
+      @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dot dot = (Dot) o;
+        return Double.compare(dot.x, x) == 0 && Double.compare(dot.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
